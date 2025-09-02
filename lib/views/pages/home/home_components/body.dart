9a8fa64c1124632d0044bcd6a8dbab5fddd7de46
Denis Coder <dnxcoder@gmail.com>
2025-09-02 13:04:30 -0500

    implemented new pictures and implemented scale effect

--- lib/views/pages/home/home_components/body.dart
+++ lib/views/pages/home/home_components/body.dart
@@ -12,19 +12,25 @@ class Body extends StatefulWidget {
   }
 }
 
-class _Body extends State<Body> with SingleTickerProviderStateMixin {
+class _Body extends State<Body> with TickerProviderStateMixin {
   double _fromScrollDx = 0;
   double _toScrollDx = 0;
+  int _currentIndex = 0;
 
   late AnimationController _controllCarrousel;
   late final AnimationController _circleController;
+  late final AnimationController _decorationImageController;
 
   @override
   void initState() {
     super.initState();
     _circleController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 600),
+      duration: const Duration(milliseconds: 2000),
+    );
+    _decorationImageController = AnimationController(
+      vsync: this,
+      duration: const Duration(microseconds: 400),
     );
   }
 
@@ -36,12 +42,48 @@ class _Body extends State<Body> with SingleTickerProviderStateMixin {
     final BODYHEIGHT = HEIGHT - APPBARPORCENTAGE;
 
     final myList = [
-      Item(width: WIDTH, imageSrc: "woman-hall.png", index: 0),
-      Item(width: WIDTH, imageSrc: "ghorst.png", index: 1),
-      Item(width: WIDTH, imageSrc: "corpsebridge.png", index: 2),
-      Item(width: WIDTH, imageSrc: "woman-hall.png", index: 3),
-      Item(width: WIDTH, imageSrc: "ghorst.png", index: 4),
-      Item(width: WIDTH, imageSrc: "corpsebridge.png", index: 5),
+      Item(
+        name: "Spooky Doll",
+        price: 209.99,
+        width: WIDTH,
+        imageSrc: "woman-hall.png",
+        index: 0,
+      ),
+      Item(
+        name: "Pumpkin\nKing",
+        price: 209.99,
+        width: WIDTH,
+        imageSrc: "ghorst.png",
+        index: 1,
+      ),
+      Item(
+        name: "La Muerta",
+        price: 209.99,
+        width: WIDTH,
+        imageSrc: "corpsebridge.png",
+        index: 2,
+      ),
+      Item(
+        name: "Skeleton\nGirl",
+        price: 209.99,
+        width: WIDTH,
+        imageSrc: "skeleton-girl2.png",
+        index: 3,
+      ),
+      Item(
+        name: "Burger\nBandit",
+        price: 209.99,
+        width: WIDTH,
+        imageSrc: "hamburger-man2.png",
+        index: 4,
+      ),
+      Item(
+        name: "Dragon Girl",
+        price: 209.99,
+        width: WIDTH,
+        imageSrc: "child-dragon2.png",
+        index: 5,
+      ),
     ];
 
     debugPrint("ANYWAY $APPBARPORCENTAGE");
@@ -62,9 +104,13 @@ class _Body extends State<Body> with SingleTickerProviderStateMixin {
           Stack(
             children: [
               Positioned(
-                bottom: BODYHEIGHT * 0.7 * 0.3,
-                right: WIDTH * 0.05,
-                child: Circle(controller: _circleController).animate(),
+                bottom: BODYHEIGHT * 0.7 * 0.05,
+                right: -40,
+                child: Circle(
+                  controller: _circleController,
+                  currentIndex: _currentIndex,
+                  decorationImageController: _decorationImageController,
+                ),
               ),
               Container(
                 height: BODYHEIGHT * 0.7,
@@ -101,19 +147,24 @@ class _Body extends State<Body> with SingleTickerProviderStateMixin {
   void moveCarouselFoward(
     AnimationController carrouselController,
     AnimationController circleController,
+
     double offsetX,
   ) {
     setState(() {
       _fromScrollDx = _toScrollDx;
       _toScrollDx -= offsetX;
+      _currentIndex += 1;
     });
 
     carrouselController.forward(from: 0);
+    _decorationImageController.forward(from: 0);
     circleController.toggle();
   }
 }
 
 class Item extends StatelessWidget {
+  String name;
+  double price;
   double width;
   String imageSrc;
   int index;
@@ -123,29 +174,42 @@ class Item extends StatelessWidget {
     required this.width,
     required this.imageSrc,
     required this.index,
+    required this.name,
+    required this.price,
   });
 
   Widget leftImageRow(BoxConstraints constaints) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        SizedBox(
-          height: constaints.maxHeight * 0.7,
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: constaints.maxHeight * 0.8,
+            maxWidth: width * .6,
+          ),
           child: Image.asset("assets/images/$imageSrc", height: 400),
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "La Muerta",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "\$ 200.99",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            ElevatedButton(onPressed: () => {}, child: Text("More ->")),
-          ],
+        Transform.translate(
+          offset: const Offset(0, -100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "\$200.99",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: Text("More ->", style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
         ),
       ],
     );
@@ -155,22 +219,34 @@ class Item extends StatelessWidget {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "La Muerta",
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "\$ 200.99",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            ElevatedButton(onPressed: () => {}, child: Text("More ->")),
-          ],
+        Transform.translate(
+          offset: Offset(20, -200),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "\$ 200.99",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: Text("More ->", style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
         ),
-        SizedBox(
-          height: constaints.maxHeight * 0.7,
+        ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: constaints.maxHeight * 0.7,
+            maxWidth: width * 0.6,
+          ),
+
           child: Image.asset("assets/images/$imageSrc", height: 400),
         ),
       ],
--- lib/views/pages/home/home_components/circle.dart
+++ lib/views/pages/home/home_components/circle.dart
@@ -3,8 +3,15 @@ import 'package:flutter_animate/flutter_animate.dart';
 
 class Circle extends StatefulWidget {
   final AnimationController controller;
+  final AnimationController decorationImageController;
+  final int currentIndex;
 
-  const Circle({super.key, required this.controller});
+  const Circle({
+    super.key,
+    required this.controller,
+    required this.currentIndex,
+    required this.decorationImageController,
+  });
 
   @override
   State<Circle> createState() {
@@ -14,28 +21,96 @@ class Circle extends StatefulWidget {
 }
 
 class _Circle extends State<Circle> {
+  final List<String> listOfImages = [
+    "castle2.png",
+    "pumpkin2.png",
+    "lamuerta4.png",
+    "skeleton7.png",
+    "hamb3.png",
+    "dragon2.png",
+  ];
+
+  String selectedImageSrc = "castle.png";
+
   @override
   Widget build(BuildContext context) {
-    double _fromDxCircle = 0;
-    double _toDxCircle = 0;
-
-    double _fromDyCircle = 0;
-    double _toDyCircle = 0;
-
     AnimationController circleController = widget.controller;
+    AnimationController _decorationImageController =
+        widget.decorationImageController;
+    int currentIndex = widget.currentIndex;
+
+    final down = 1000.ms;
+    final up = 1500.ms;
 
     // TODO: implement build
-    return Container(
-          width: 250,
-          height: 250,
-          decoration: const BoxDecoration(
-            color: Colors.deepOrange,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(200),
-              topRight: Radius.circular(200),
-              bottomLeft: Radius.circular(200),
-              bottomRight: Radius.circular(200),
+    return ClipOval(
+          child: Container(
+            width: 300,
+            height: 300,
+            decoration: const BoxDecoration(
+              color: Colors.deepOrange,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(200),
+                topRight: Radius.circular(200),
+                bottomLeft: Radius.circular(200),
+                bottomRight: Radius.circular(200),
+              ),
             ),
+            child:
+                Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image.asset(
+                        "assets/images/${selectedImageSrc}",
+                        width: 270,
+                      ),
+                    )
+                    /// CANAL 1: movimento (desce e depois sobe)
+                    .animate(
+                      controller: _decorationImageController,
+                      autoPlay: false,
+                      onPlay: (c) => c.forward(from: 0),
+                    )
+                    .moveY(
+                      duration: down,
+                      begin: 0,
+                      end: 400,
+                      curve: Curves.easeIn,
+                    )
+                    .then()
+                    .callback(
+                      duration: 0.ms,
+                      callback: (_) {
+                        setState(() {
+                          selectedImageSrc = listOfImages[currentIndex];
+                        });
+                      },
+                    )
+                    .moveY(
+                      duration: up,
+                      begin: 0,
+                      end: -400,
+                      curve: Curves.fastOutSlowIn,
+                    )
+                    // CANAL 2: escala + troca de imagem, tudo em sequência
+                    .animate(
+                      controller: _decorationImageController,
+                      autoPlay: false,
+                    )
+                    .scaleXY(
+                      duration: down,
+                      begin: 1,
+                      end: 0.5,
+                      curve: Curves.easeOut,
+                      alignment: Alignment.bottomCenter,
+                    )
+                    .then()
+                    .scaleXY(
+                      duration: up,
+                      begin: 1,
+                      end: 2,
+                      curve: Curves.easeOut,
+                      alignment: Alignment.bottomCenter,
+                    ),
           ),
         )
         .animate(controller: circleController, autoPlay: false)
